05b3dc00f15879157de17c671f450508136c6016
Darren Craig <darren@minus40.co>
2014-09-07 19:59:17 +0100

    Adding some basic validation to the date of birth value object

--- src/Core/ValueObjects/DateOfBirth.php
+++ src/Core/ValueObjects/DateOfBirth.php
@@ -1,5 +1,6 @@
 <?php namespace Iceflow\Core\ValueObjects;
 
+use Config;
 use DateTime;
 use Doctrine\ORM\Mapping AS ORM;
 use Iceflow\Core\ValueObjects\Exceptions\InvalidDateOfBirthException;
@@ -10,28 +11,69 @@ use Iceflow\Core\ValueObjects\Exceptions\InvalidDateOfBirthException;
 class DateOfBirth
 {
     /**
-     * @ORM\Column(type = "date")
+     * @ORM\Column(type = "date", name = "date")
      */
     public $date;
 
+    /**
+     * @param $date
+     * @throws InvalidDateOfBirthException
+     */
     public function __construct($date)
     {
-        // TODO: Validate the date properly.
-        if(false) throw InvalidDateOfBirthException();
+        $this->format = Config::get('iceflow.date-format');
+        $this->date = DateTime::createFromFormat($this->format, $date);
 
-        $this->date = new DateTime($date);
+        $this->checkDateIsValid($date);
     }
 
+    /**
+     * @return string
+     */
     public function getDay(){
-        return date("d", $this->date);
+        return $this->date->format( 'd' );
     }
 
+    /**
+     * @return string
+     */
     public function getMonth(){
-        return date("m", $this->date);
+        return $this->date->format( 'm' );
     }
 
+    /**
+     * @return string
+     */
     public function getYear(){
-        return date("Y", $this->date);
+        return $this->date->format( 'Y' );
+    }
+
+    /**
+     * @return bool
+     * @throws InvalidDateOfBirthException
+     */
+    private function checkDateIsValid($date)
+    {
+        if($this->isInThePast() && $this->isValidFormat($date)) return true;
+
+        throw new InvalidDateOfBirthException;
+    }
+
+    /**
+     * @param $date
+     * @return bool
+     */
+    private function isValidFormat($date) {
+        return $this->date && $this->date->format($this->format) == $date;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isInThePast()
+    {
+        $diff = (new DateTime())->diff($this->date);
+        return $diff->format('%R%a') < 0;
     }
 
     /**
